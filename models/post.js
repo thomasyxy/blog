775a92485bd845c8572a0a957c4b82d3520314a2
yinxueyuan <838476901@qq.com>
2016-03-04 17:52:49 +0800

    3.4

--- models/post.js
+++ models/post.js
@@ -286,8 +286,8 @@ Post.getTen = function(name, page, callback){
 			collection.count(query, function(err, total){
 				//根据query对象查询，并跳过前（page-1）*10个结果，返回之后的10个结果
 				collection.find(query,{
-					skip:(page - 1)*10,
-					limit: 10
+					skip:(page - 1)*5,
+					limit: 5
 				}).sort({
 					time: -1
 				}).toArray(function(err, docs){
